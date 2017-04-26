e9fbdcfbc731de64e8dc143cfeb5ce766a31889b
RonildoSouza <ronildo_rns@hotmail.com>
2017-04-26 15:25:14 -0300

    ajustes

--- _UDFs/Documentação UDFs/js/teste.js
+++ _UDFs/Documentação UDFs/js/teste.js
@@ -1,12 +1,22 @@
-function carregaJson() {
-    var j = JSON.parse(udf_auxiliares);
-    var str = '';
+function getDadosComoObjeto(param) {
+    // var j = JSON.parse(udfs_auxiliares);
+    var objJsonParse = JSON.parse(param);
+    
+    // var str = '';
+    // for (var index = 0; index < objJsonParse.length; index++) {
+    //     str += '\n' + objJsonParse[index].titulo + '\n';
 
-    for (var index = 0; index < j.length; index++) {
-        str += j[index].titulo + '\n';        
-    }
+    //     for (var i = 0; i < objJsonParse[index].funcoes.length; i++) {
+    //         str += '\t' + (i + 1) + ' - ' + objJsonParse[index].funcoes[i] + '\n';            
+    //     }       
+    // }
+    // console.log(str);
 
-    alert(str);
+    return objJsonParse;
 }
 
-carregaJson();
+function montaTabela(param){
+    var dados = getDadosComoObjeto(param);
+}
+
+// montaTabela(udfs_guardian);

077aa3ebf230cb59bb4c8dcc15519287a63d8cb2
RonildoSouza <ronildo_rns@hotmail.com>
2017-04-28 15:54:28 -0300

    ajustes doc udf

--- _UDFs/Documentação UDFs/js/teste.js
+++ _UDFs/Documentação UDFs/js/teste.js
@@ -1,66 +1,59 @@
-function getDadosComoObjeto(param) {
-    try{
-        var objJsonParse = JSON.parse(param); 
-        return objJsonParse;
-    }
-    catch(e){
-        console.log("erro: " + e);
+function getDadosComoObjeto(strJsonUdf) {    
+    if(strJsonUdf){
+        try{
+            var objJsonParse = JSON.parse(strJsonUdf); 
+            return objJsonParse;        
+        }
+        catch(ex){
+            console.error(ex.message);
+        }
     }
 }
 
-function montaTabela(param){
-    var dados = getDadosComoObjeto(param);
+function montaTabela(strJsonUdf){
+    var dados = getDadosComoObjeto(strJsonUdf);
+    var tbody = document.getElementById("tbody");    
 
-    var tbody = document.getElementById("tbody");	
+    if(dados && tbody){
+        
+        if(tbody && tbody.childNodes.length > 0){
+            tbody.remove();
+            tbody = document.createElement("tbody");
+            tbody.setAttribute('id', 'tbody');
 
-    for (var i = 0; i < dados.length; i++) {
-        var tr1 = document.createElement("tr");
+            document.getElementById("table").appendChild(tbody);
+        }
 
-        var td_udf = document.createElement("td");
-        td_udf.setAttribute('align', 'center');
-        var txt1 = document.createTextNode(dados[i].titulo);
-        td_udf.appendChild(txt1);
+        for (var i = 0; i < dados.length; i++) {
+            var tr1 = document.createElement("tr");
+            var arrayTexto = [dados[i].titulo, dados[i].pasta, dados[i].versao]
 
-        var td_pasta = document.createElement("td");
-        td_pasta.setAttribute('align', 'center');
-        var txt2 = document.createTextNode(dados[i].pasta);
-        td_pasta.appendChild(txt2);
+            var tr = criaLinhaColuna(arrayTexto, dados[i]);
+            tbody.appendChild(tr);
 
-        var td_versao = document.createElement("td");
-        td_versao.setAttribute('align', 'center');
-        var txt3 = document.createTextNode(dados[i].versao);
-        td_versao.appendChild(txt3);
-
-        tr1.appendChild(td_udf);
-        tr1.appendChild(td_pasta);
-        tr1.appendChild(td_versao);        
-
-        tbody.appendChild(tr1);
-
-        for (var j = 0; j < dados[i].funcoes.length; j++) { 
-            var tr2 = document.createElement("tr");
-
-            var td_funcoes = document.createElement("td");
-            td_funcoes.setAttribute('colspan', 3);            
-            var txt4 = document.createTextNode(dados[i].funcoes[j]);
-            td_funcoes.appendChild(txt4);
-
-            tr2.appendChild(td_funcoes);
-            tbody.appendChild(tr2);
-        }        
-     }
+            // for (var j = 0; j < dados[i].funcoes.length; j++) {
+            //     var tr = criaLinhaColuna([dados[i].funcoes[j]], 'colspan', 3);
+            //     tbody.appendChild(tr);
+            // }        
+        }
+    }
 }
 
-// {'+
-//     '"titulo": "GeraCPF_CNPJ.au3",'+
-//     '"descricao": "UDF para gerar CPF e CNPJ de forma aleatória",'+
-//     '"pasta": "_UDFs\Auxiliares",'+
-//     '"versao": "003",'+
-//     '"autores": ['+
-//         '"Ronildo"'+
-//     '],'+
-//     '"funcoes": ['+
-//         '"GerarCPF($bPontos = False)",'+
-//         '"GerarCNPJ($bPontos = False)"'+
-//     ']'+
-// '},'+
+function criaLinhaColuna(arrayTextNode, obj, tdAttr = 'align', tdAttrValue = 'center') {
+    var tr = document.createElement("tr");
+
+    for (var i = 0; i < arrayTextNode.length; i++) {  
+        var td = document.createElement("td");    
+        td.setAttribute(tdAttr, tdAttrValue);
+        var txt1 = document.createTextNode(arrayTextNode[i]);
+
+        td.appendChild(txt1); 
+        tr.appendChild(td);     
+    }
+    return tr;
+}
+
+function teste(a){ 
+    // alert(a);
+    console.debug(a.versao);
+}
